472ed0f6f6cebe6b4e0a6a6d226b63cecbc1410a
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-09-01 13:31:44 +0300

    dynamic class

---
+++ dynamic-class/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    // template: "<h1>I am samundra Dhakal</h1>"
+    data(){
+        return{
+            url: "https://www.facebook.com",
+            showBooks: true,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav:true},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpeg', isFav:false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpeg', isFav:true },
+              ],
+        }
+    },
+    methods: {
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        toggleFav(item){
+            item.isFav = !item.isFav
+        }
+    }
+})
+
+
+app.mount("#app");
